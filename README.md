fc02ccee361b5e057ae5c1c89eb2c9fa8e8fb43f
shooooori0128 <shori.nishizawa@x-factory.jp>
2022-09-15 10:35:00 +0900

    initialize :tada:

---
+++ README.md
@@ -0,0 +1,5 @@
+# monorepo
+
+下記リポジトリを参考にNuxtのモノレポ構成を作成
+
+<https://github.com/kazupon/nuxt2-monorepo-example/tree/46e732a39ba11daa8ecf855d1e6921a7e9875c9a>
---
+++ packages/components/src/index.js
@@ -0,0 +1,3 @@
+export { default as NuxtLogo } from './organisms/NuxtLogo.vue';
+export { default as Tutorial } from './organisms/Tutorial.vue';
+export { default as VuetifyLogo } from './organisms/VuetifyLogo.vue';
